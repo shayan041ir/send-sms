236504a72be6317dee7725d5b01c46dae0382ce0
ShayanRezayi <135282886+shayan041ir@users.noreply.github.com>
2024-11-24 17:15:37 +0330

    Update index.php

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ use Kavenegar\KavenegarApi;  // فضای نام درست
 $sender = "1000689696";
 $receptor = "09146491018";
 $message = "وب سرویس پیام کوتاه کاوه نگار";
-$apiKey = "444F62484B5A6C505754447738312F3662314E5948456468667974456A786D5276454A79726C7047334F593D"; // کلید API
+$apiKey = "your api key"; // کلید API
 
 // ساخت شی از کلاس KavenegarApi
 $api = new KavenegarApi($apiKey);
